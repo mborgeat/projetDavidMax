ba1a4f032ad9e1bf59c334a31b870681a7257911
Valdensis <david.kcam@gmail.com>
2016-10-10 21:51:41 +0200

    Test unitaire sur Prewitt
    Ajouté un test sur cette méthode

--- TestMaxDavid/Properties/Resources.Designer.cs
+++ TestMaxDavid/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace TestMaxDavid.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap firefox_night_prewitt {
+            get {
+                object obj = ResourceManager.GetObject("firefox_night_prewitt", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- TestMaxDavid/UnitTest1.cs
+++ TestMaxDavid/UnitTest1.cs
@@ -18,10 +18,9 @@ namespace ImageEdgeDetection
             Bitmap test = ImageFilters.ApplyFilter(new Bitmap(nonFiltree), 1, 1, 1, 25);
 
             // Tester la taille de l'image
-            Assert.AreEqual(test.Width, reference.Width);
             Assert.AreEqual(test.Size, reference.Size);
 
-            // Boucles sur les pixels
+            // Boucles sur les pixels; cela a du sens puisque les deux images ont la même taille
             for (int i = 0; i < test.Width; i++)
             {
                 for (int j = 0; j < test.Height; j++)
@@ -29,6 +28,27 @@ namespace ImageEdgeDetection
                     Assert.AreEqual(reference.GetPixel(i, j), test.GetPixel(i, j));
                 }
             }
+        }
+
+        
+        [TestMethod]
+        public void TestPrewitt()
+        {
+            // Image de départ
+            Bitmap nonFiltree = new Bitmap(TestMaxDavid.Properties.Resources.firefox_night);
+            // Image filtrée auparavant, servant de référence
+            Bitmap reference = new Bitmap(TestMaxDavid.Properties.Resources.firefox_night_prewitt);
+            // Utilisation de la méthode; l'image 'test' devrait être identique à l'image 'reference'
+            Bitmap test = nonFiltree.PrewittFilter(false);
+
+            // Tester la taille de l'image
+            Assert.AreEqual(reference.Size, test.Size);
+
+            // Boucles sur les pixels
+            for (int i = 0; i < test.Width; i++)
+                for (int j = 0; j < test.Height; j++)
+                    Assert.AreEqual(reference.Size, test.Size);
+
         }
     }
 }
