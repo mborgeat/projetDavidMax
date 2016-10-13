79e351f651a912520b0cc21a16797589384fcb88
Valdensis <david.kcam@gmail.com>
2016-10-13 16:12:39 +0200

    Ajout des tests sur les tailles
    Pour les méthodes ApplyFilter et PrewittFilter

--- TestMaxDavid/UnitTest1.cs
+++ TestMaxDavid/UnitTest1.cs
@@ -8,6 +8,21 @@ namespace ImageEdgeDetection
     public class UnitTest1
     {
         [TestMethod]
+        public void TestNightFilterSize()
+        {
+            // Test de la méthode ApplyFilter (filtre 'Night Filter'). L'image de retour doit avoir la même taille que celle de départ.
+
+            // Image de départ
+            Bitmap nonFiltree = new Bitmap(TestMaxDavid.Properties.Resources.firefox);
+            
+            // Image filtrée
+            Bitmap test = ImageFilters.ApplyFilter(new Bitmap(nonFiltree), 1, 1, 1, 25);
+
+            // Test : on compare les tailles des images de départ et d'arrivée.
+            Assert.AreEqual(nonFiltree.Width, test.Width);
+            Assert.AreEqual(nonFiltree.Height, test.Height);
+        }
+        [TestMethod]
         public void TestNightFilter()
         {
             // Test de la méthode ApplyFilter pour le filtre 'Night Filter' avec images de départ et d'arrivée
@@ -19,7 +34,7 @@ namespace ImageEdgeDetection
             // Utilisation de la méthode; l'image 'test' devrait être identique à l'image 'reference'
             Bitmap test = ImageFilters.ApplyFilter(new Bitmap(nonFiltree), 1, 1, 1, 25);
 
-            // Tester la taille de l'image
+            // Tester la taille de l'image; si pas égale, il ne sera pas possible de boucler sur lignes et colonnes
             Assert.AreEqual(test.Size, reference.Size);
 
             // Boucles sur les pixels; cela a du sens puisque les deux images ont la même taille
@@ -31,8 +46,33 @@ namespace ImageEdgeDetection
                 }
             }
         }
+        [TestMethod]
+        public void TestNightFilterNull()
+        {
+            // Test de la méthode ApplyFilter (pour le filtre 'night filter') avec une image nulle
 
-        
+            // Image de départ nulle
+            Bitmap nonFiltree = null;
+
+            // Test de la méthode sur une image nulle : doit renvoyer null
+            Assert.AreEqual(null, ImageFilters.ApplyFilter(nonFiltree, 1, 1, 1, 25));
+        }
+        [TestMethod]
+        public void TestPrewittSize()
+        {
+            // Test de la méthode Prewitt. L'image de retour doit avoir la même taille que celle de départ.
+
+            // Image de départ
+            Bitmap nonFiltree = new Bitmap(TestMaxDavid.Properties.Resources.firefox);
+
+            // Image filtrée
+            Bitmap test = nonFiltree.PrewittFilter(false);
+
+            // Test : on compare les tailles des images de départ et d'arrivée.
+            Assert.AreEqual(nonFiltree.Width, test.Width);
+            Assert.AreEqual(nonFiltree.Height, test.Height);
+        }
+
         [TestMethod]
         public void TestPrewitt()
         {
@@ -45,7 +85,7 @@ namespace ImageEdgeDetection
             // Utilisation de la méthode; l'image 'test' devrait être identique à l'image 'reference'
             Bitmap test = nonFiltree.PrewittFilter(false);
 
-            // Tester la taille de l'image
+            // Tester la taille de l'image. Si pas égale, il ne sera pas possible de boucler sur lignes et colonnes
             Assert.AreEqual(reference.Size, test.Size);
 
             // Boucles sur les pixels
@@ -54,18 +94,7 @@ namespace ImageEdgeDetection
                     Assert.AreEqual(reference.Size, test.Size);
 
         }
-        [TestMethod]
-        public void TestNightFilterNull()
-        {
-            // Test de la méthode ApplyFilter (pour le filtre 'night filter') avec une image nulle
-
-            // Image de départ nulle
-            Bitmap nonFiltree = null;
-            
-            // Test de la méthode sur une image nulle : doit renvoyer null
-            Assert.AreEqual(null, ImageFilters.ApplyFilter(nonFiltree, 1, 1, 1, 25));
-        }
-
+        
         [TestMethod]
         public void TestPrewittNull()
         {
